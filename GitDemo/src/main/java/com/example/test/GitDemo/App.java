fd00bb22d24ff264d25de3b181de3342d1f1aa12
Mark Bidewell <bidewell.mark@pennmutual.com>
2015-11-06 16:01:23 -0500

    commit change

--- GitDemo/src/main/java/com/example/test/GitDemo/App.java
+++ GitDemo/src/main/java/com/example/test/GitDemo/App.java
@@ -10,6 +10,6 @@ public class App
     {
         System.out.println( "Hello World! This change is made by Srini :)" );
 		System.out.println( "Ramji 1 :)" );
-		System.out.println( "Mark Bidewell 123" );
+		System.out.println( "Mark Bidewell 123 Test is a Branch Test" );
     }
 }
