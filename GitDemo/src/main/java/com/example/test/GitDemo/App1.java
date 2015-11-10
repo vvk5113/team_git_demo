82a4ad1bd2a0b634e5201701796b907908798b6f
Mark Bidewell <bidewell.mark@pennmutual.com>
2015-11-10 14:53:05 -0500

    Support for Matchbox

--- GitDemo/src/main/java/com/example/test/GitDemo/App1.java
+++ GitDemo/src/main/java/com/example/test/GitDemo/App1.java
@@ -4,12 +4,13 @@ package com.example.test.GitDemo;
  * Hello world!
  *
  */
-public class App1 
+public class App1
 {
     public static void main( String[] args )
     {
         System.out.println( "PRETTY" );
 		System.out.println( "Hot wheels on the road" );
 		System.out.println( "PRETTY" );
+		System.out.println("Support for Matchbox");
     }
 }
